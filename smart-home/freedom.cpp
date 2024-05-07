6b547dfd1b341cfcaccc1e782e91f93ab7171115
Thea <thea.h.w.verburg@gmail.com>
2024-05-07 12:26:11 +0200

    Pushing the full thing

--- smart-home/freedom.cpp
+++ smart-home/freedom.cpp
@@ -2,7 +2,13 @@
 #include "adc.h"
 #include <cstdint>
 #include <cstdio>
+#include "oled_ssd1322.h"
 #include "home.cpp"
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define MESSAGE_MAX_SIZE 50
 
 class FreedomController {
     public:
@@ -11,42 +17,149 @@ class FreedomController {
         DigitalIn Blue_Button;
         DigitalIn Black_Button;// switch button
         DigitalIn Red_Button; // switch button
+
+        DigitalOut Red_Led;
+        DigitalOut Green_Led;
         FreedomController(
             PinName green_button_pin,
             PinName blue_button_pin,
             PinName black_button_pin,
             PinName red_button_pin,
+            PinName red_led_pin,
+            PinName green_led_pin,
             Home& homeRef
             ) : 
             Green_Button(green_button_pin),
             Blue_Button(blue_button_pin),
             Black_Button(black_button_pin),
             Red_Button(red_button_pin),
+            Red_Led(red_led_pin),
+            Green_Led(green_led_pin),
             SmartHome(homeRef) {}
 
         void init() {
+            InitScreen();
+            // SmartHome.SetHeaterOn();
+            SmartHome.Front_Door = 1;
             while (true)
             {
-                bool openDoor = false;
-                if(!Blue_Button && !openDoor){
-                    SmartHome.Curtains = 1;
-                    openDoor = true;
-                    ThisThread::sleep_for(1000ms);
-                }
-                else if (!Blue_Button && openDoor){
-                    SmartHome.Curtains = 0;
-                    openDoor = false;
-                    ThisThread::sleep_for(1000ms);
+                // Smarthome actions:
+                    // UC_Too_Warm // DONE
+                    // When it’s above 30 degrees celsius (inside-sensor), make sure the heater. 
+                    // The red light of the freedom controller will turn red if it is above 30 degrees.
+
+                    // UC_Sunset // done
+                    // When the light sensor detects that it is dark (< 10'000), turn on all the lights in- and outside the house.
+                    // 
+                    // UC_Sunrise //  done
+                    // When the light sensor detects that it is daylight (>10'000), turn off all the lights in- and outside the house.
+                    
+                    // UC_HomeComing //done?
+                    // When the car is at the garage door(car sensor), open the garage and turn on the heater if the inside temperature sensor is below 25 degrees.
+                    // Also we turn on the inside light when this occurs.
+                    
+                    // UC_DeliverPackage //done 
+                    // When the green button on the freedom controller is pressed, the door is opened for 5 seconds, “so a package can be delivered while you’re not at home”.
+
+
+                if (IsItTooWarm()) {
+                    SmartHome.SetHeaterOff();
+                    Red_Led = 1;
+                } else {
+                    Red_Led = 0;
                 }
 
-                if (SmartHome.Sensors.Car_Sensor == 0) {
+                if (IsCarOnDriveway()) {
                     ThisThread::sleep_for(1s);
-                    // SmartHome.Curtains = 1;
-                    SmartHome.Garage_Door = 1; // does not work?
+                    SmartHome.Garage_Door = 1;
+                    SmartHome.Curtains = 0; // bug, should not open because of this event.
                     ThisThread::sleep_for(5s);
-                    // SmartHome.Curtains = 0;
                     SmartHome.Garage_Door = 0; 
+                    SmartHome.SetIndoorLampOn();
+                    
+
+                    if (IsItBelowDegrees(25.0)) {
+                        SmartHome.SetHeaterOn();
+                    }
                 }
+
+                if (!Green_Button) {
+                    SmartHome.Front_Door = 0;
+                    ThisThread::sleep_for(5s);
+                    SmartHome.Front_Door = 1;
+                }
+
+                // bug timeeee
+                if (!Blue_Button) {
+                    while (true) {
+                        SmartHome.Garage_Door = 1;
+                        SmartHome.Garden_Lamp = 1;
+                        SmartHome.SetFrontDoorLampOn();
+                        ThisThread::sleep_for(100ms);
+                        SmartHome.Garden_Lamp = 0;
+                        SmartHome.SetFrontDoorLampOff();
+                        ThisThread::sleep_for(100ms);
+                    }
+                }
+
+                if (SmartHome.Sensors.IsDarkOutside()) {
+                    SmartHome.Garden_Lamp = 1;
+                     SmartHome.Curtains = 0;
+                } else {
+                    SmartHome.Garden_Lamp = 0;
+                    SmartHome.Curtains = 1;
+                }
+
+                UpdateDashboard();
+                ThisThread::sleep_for(500ms);
             }
+        }
+
+        void InitScreen() {
+            u8g2_ClearBuffer(&oled);
+            u8g2_SetFont(&oled, u8g2_font_6x12_mr);
+            u8g2_SendBuffer(&oled);
+        }
+
+        void UpdateDashboard() {
+            u8g2_ClearBuffer(&oled);
+            // info
+            int light_val_outside = SmartHome.Sensors.GetAnalogValueOfOutdoorLight();
+            float temperature = SmartHome.Sensors.getInsideTemperature();
+
+            char message1[MESSAGE_MAX_SIZE + 1];
+            message1[MESSAGE_MAX_SIZE] = '\0';
+            snprintf(message1, MESSAGE_MAX_SIZE, "========SMART HOME========\n");
+
+            char message2[MESSAGE_MAX_SIZE + 1];
+            message2[MESSAGE_MAX_SIZE] = '\0';
+            snprintf(message2, MESSAGE_MAX_SIZE, "Temperature inside: %5.02f\n" , temperature);
+            
+            char message3[MESSAGE_MAX_SIZE +1];
+            message3[MESSAGE_MAX_SIZE] = '\0';
+            snprintf(message3, MESSAGE_MAX_SIZE,  "Light-value outside:  %d \n" ,light_val_outside);
+
+            SetMessage(message1, 10, 10);
+            SetMessage(message2, 10, 20);
+            SetMessage(message3, 10, 30);
+            u8g2_SendBuffer(&oled);
+        }
+
+
+        void SetMessage(char* msg_string, int x, int y) {
+            // u8g2_ClearBuffer(&oled);
+            u8g2_DrawUTF8(&oled, x, y, msg_string);
+        }
+
+        bool IsItTooWarm() {
+            return SmartHome.Sensors.getInsideTemperature() > 30.0;
+        }
+
+        bool IsItBelowDegrees(float degrees) {
+            return SmartHome.Sensors.getInsideTemperature() < degrees;
+        }
+
+        bool IsCarOnDriveway() {
+            return SmartHome.Sensors.Car_Sensor == 0;
         }
 };
--- smart-home/main.cpp
+++ smart-home/main.cpp
@@ -1,50 +1,49 @@
-#include "mbed.h"
-#include "board_freedom.h"
 #include "adc.h"
-#include "oled_ssd1322.h"
+#include "board_freedom.h"
+#include "freedom.cpp"
+#include "mbed.h"
 #include <cstdint>
 #include <cstdio>
-#include "freedom.cpp"
 
-#define MESSAGE_MAX_SIZE 50
- 
+
+
 //  /*Libraries to add:::
-// Select ‘master’ for u8g2 and ‘main’ for libboxzoo in the next window. Hit finish and add the other library.
+// Select ‘master’ for u8g2 and ‘main’ for libboxzoo in the next window. Hit
+// finish and add the other library.
 // https://github.com/valuta1995/bugzoo-u8g2.git
 // https://github.com/valuta1995/libboxzoo.git
 
+int main() {
+  board_init();
 
-int main()
-{
-   board_init();
+  HomeSensors Smart_Home_Sensors(PTE25, PTA8, PTA9, 0, 1);
+  Home Smart_Home(PTB18, PTB11, PTE24, PTC12, PTC7, Smart_Home_Sensors);
+  FreedomController Freedom(PTB5, PTB6, PTC16, PTB7, PTB2, PTB3, Smart_Home);
 
-    HomeSensors Smart_Home_Sensors(PTE25, PTA8, PTA9, 0, 1);
-    Home Smart_Home(PTB18,PTB11,PTE24,PTC12,PTC7, Smart_Home_Sensors);
-    FreedomController Freedom(PTB5, PTB6, PTC16, PTB7, Smart_Home);
+  Freedom.init();
 
-    Freedom.init();
+  // SMART HOME NR 5 ZIGGY GLITTERDUST - 23-04-2024
 
-    // SMART HOME NR 5 ZIGGY GLITTERDUST - 23-04-2024
+  // NOT Implemented yet:
+  // uint16_t analog_in_value = adc_read(2);// AN Spare/blue at runtime
+  // uint16_t analog_in_value = adc_read(3);// IO/AN Spare/purple at runtime
 
-    // NOT Implemented yet:
-    // uint16_t analog_in_value = adc_read(2);// AN Spare/blue at runtime
-    // uint16_t analog_in_value = adc_read(3);// IO/AN Spare/purple at runtime
+  // char message[MESSAGE_MAX_SIZE + 1];
+  // message[MESSAGE_MAX_SIZE] = '\0';
 
-    // char message[MESSAGE_MAX_SIZE + 1];
-    // message[MESSAGE_MAX_SIZE] = '\0';
+  // u8g2_ClearBuffer(&oled);
+  // u8g2_SetFont(&oled, u8g2_font_6x12_mr);
+  // u8g2_SendBuffer(&oled);
 
-    // u8g2_ClearBuffer(&oled);
-    // u8g2_SetFont(&oled, u8g2_font_6x12_mr);
-    // u8g2_SendBuffer(&oled);
+  // snprintf(message, MESSAGE_MAX_SIZE, "Value is %-5d, temperature is %5.02f
+  // \n", analog_in_value , temperature);
 
+  // Clear screen and write a message.
+  // u8g2_ClearBuffer(&oled);
+  // u8g2_DrawUTF8(&oled, 10, 10, message);
+  // u8g2_SendBuffer(&oled);
 
-        // snprintf(message, MESSAGE_MAX_SIZE, "Value is %-5d, temperature is %5.02f \n", analog_in_value , temperature);
-
-        // Clear screen and write a message.
-        // u8g2_ClearBuffer(&oled);
-        // u8g2_DrawUTF8(&oled, 10, 10, message);
-        // u8g2_SendBuffer(&oled);
-        
-        // Also write the message over USB to the serial monitor
-        // printf("Value is %-5d, temperature is %5.02f \n", analog_in_value , temperature);
+  // Also write the message over USB to the serial monitor
+  // printf("Value is %-5d, temperature is %5.02f \n", analog_in_value ,
+  // temperature);
 }
